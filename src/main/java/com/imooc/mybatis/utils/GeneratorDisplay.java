41305d90aab9e68c6aee49dff9dd435f499d437c
sunshuai <ss8795435@163.com>
2018-11-07 15:26:10 +0800

    test

--- src/main/java/com/imooc/mybatis/utils/GeneratorDisplay.java
+++ src/main/java/com/imooc/mybatis/utils/GeneratorDisplay.java
@@ -9,24 +9,22 @@ import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-
 public class GeneratorDisplay {
 
-	public void generator() throws Exception{
-
+	public void generator() throws Exception {
+		// 指定 逆向工程配置文件
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
-		//指定 逆向工程配置文件
-		File configFile = new File("generatorConfig.xml"); 
+
+		File configFile = new File("generatorConfig.xml");
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
-				callback, warnings);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
 		myBatisGenerator.generate(null);
 
-	} 
-	
+	}
+
 	public static void main(String[] args) throws Exception {
 		try {
 			GeneratorDisplay generatorSqlmap = new GeneratorDisplay();
@@ -34,6 +32,6 @@ public class GeneratorDisplay {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
